2c4b587b040161fbc664df25d822d739ca6cfb26
Mikael Blomstrand <mbloms@kth.se>
2020-03-12 13:58:24 +0100

    alternative fmap implementations

--- src/Data/ListBuilder.hs
+++ src/Data/ListBuilder.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE ExplicitForAll,
              RankNTypes #-}
 
-module Data.ListBuilder where
+module Data.ListBuilder (module Data.ListBuilder, toList) where
 
 import Data.Monoid
-import Data.Foldable hiding (toList)
+import Data.Foldable
 import Control.Applicative
 import Control.Monad
 import GHC.Exts (build, augment)
 
 newtype ListBuilder a = Build (forall b. (a -> b -> b) -> b -> b)
 
+fromList :: [a] -> ListBuilder a
 fromList xs = Build $ \cons nil -> foldr cons nil xs
 
-toList (Build f) = build f
+singleton :: a -> ListBuilder a
+singleton = return
 
 instance Semigroup (ListBuilder a) where
     (<>) = mappend
@@ -23,11 +25,19 @@ instance Monoid (ListBuilder a) where
     mappend (Build xfs) (Build yfs) = Build $ \cons -> xfs cons . yfs cons
 
 instance Functor ListBuilder where
+    -- fmap could be aliased to liftM or liftA since they don't use it.
     fmap f (Build g) = Build $ \cons nil -> g (\x xs -> cons (f x) xs) nil
+    -- Alternative implementations:
+    --fmap = liftM -- (or liftA)
+    ---- Original implementation based on foldr:
+    --fmap f lstbuilder = Build $ \cons nil -> foldr (\x xs -> cons (f x) xs) nil lstbuilder
+    --fmap f = foldMap (singleton . f)
+    --fmap f = fromList . map f . toList
 
 instance Foldable ListBuilder where
+    toList (Build f)    = build f
     foldr k z (Build g) = g k z
-    fold (Build g) =      g mappend mempty
+    fold (Build g)      = g mappend mempty
     foldMap f (Build g) = g (mappend . f) mempty
 
 instance Applicative ListBuilder where
